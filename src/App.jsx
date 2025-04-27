a63cc0272530f81aead3e570a5182c22549ef59d
aloukikattrey <aloukik@attrey.com>
2025-04-28 01:20:58 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './components/CalcArea'
 import CalcArea from './components/CalcArea';
 import './components/Head'
 import Head from './components/Head';
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Head/>
       <CalcArea/>
+      <Analytics />
     </div>
   );
 }
